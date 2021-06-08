238ccf120be9b58c1594fc43f183bd65dbf2e50d
Slavisa Djukic <slavisa.djukic@listenfirstmedia.com>
2021-06-08 14:46:55 +0000

    correct pr

--- lib/active_record/connection_adapters/redshift_adapter.rb
+++ lib/active_record/connection_adapters/redshift_adapter.rb
@@ -630,13 +630,10 @@ module ActiveRecord
         #  - ::regclass is a function that gives the id for a table name
         def column_definitions(table_name) # :nodoc:
           exec_query(<<-end_sql, 'SCHEMA').rows
-              SELECT a.attname, format_type(a.atttypid, a.atttypmod),
-                     pg_get_expr(d.adbin, d.adrelid), a.attnotnull, a.atttypid, a.atttypmod
-                FROM pg_attribute a LEFT JOIN pg_attrdef d
-                  ON a.attrelid = d.adrelid AND a.attnum = d.adnum
-               WHERE a.attrelid = '#{quote_table_name(table_name)}'::regclass
-                 AND a.attnum > 0 AND NOT a.attisdropped
-               ORDER BY a.attnum
+              SELECT column_name as attname, data_type as format_type, '' as pg_get_expr,
+                     is_nullable = 'f' as attnotnull, data_type as atttypid, -1 as atttypmod
+                FROM svv_columns
+               WHERE table_name = '#{quote_table_name(table_name)}'
           end_sql
         end
 
@@ -651,3 +648,10 @@ module ActiveRecord
     end
   end
 end
+#              SELECT a.attname, format_type(a.atttypid, a.atttypmod),
+#                     pg_get_expr(d.adbin, d.adrelid), a.attnotnull, a.atttypid, a.atttypmod
+#                FROM pg_attribute a LEFT JOIN pg_attrdef d
+#                  ON a.attrelid = d.adrelid AND a.attnum = d.adnum
+#               WHERE a.attrelid = '#{quote_table_name(table_name)}'::regclass
+#                 AND a.attnum > 0 AND NOT a.attisdropped
+#               ORDER BY a.attnum
